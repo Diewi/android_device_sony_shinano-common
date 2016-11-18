975405a61208a88df7001715856fc2036f50f105
Alexander Diewald <Diewi@diewald-net.com>
2016-11-18 22:15:27 +0100

    Move TARGET_RECOVERY_IS_MULTIROM from devices to common board cfg.
    Change-Id: I0948b70b33fa6be9bdb5f8cda3e2a2ff0325d4f8
    Signed-off-by: Alexander Diewald <Diewi@diewald-net.com>

--- BoardConfigCommonOmni.mk
+++ BoardConfigCommonOmni.mk
@@ -21,6 +21,8 @@ TARGET_RECOVERY_DEVICE_DIRS += device/sony/shinano-common
 WITH_DEXPREOPT := false
 
 #MultiROM config. MultiROM also uses parts of TWRP config
+TARGET_RECOVERY_IS_MULTIROM := true
+
 MR_INPUT_TYPE := type_b
 MR_INIT_DEVICES := device/sony/shinano-common/multirom/mr_init_devices.c
 MR_BINARY_SELECTOR := device/sony/shinano-common/multirom/mr_binary_selector.c
